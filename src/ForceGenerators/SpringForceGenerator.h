ed4cd25f47a47b1ed9d268bb22058b61287a2ef9
Ilyes Najjari <ilyes.najjari@hotmail.com>
2024-10-02 16:39:14 -0400

    Update SpringForceGenerator.h

--- src/ForceGenerators/SpringForceGenerator.h
+++ src/ForceGenerators/SpringForceGenerator.h
@@ -1,27 +1,15 @@
 #pragma once
 #include "ForceGenerator.h"
+#include "Particle.h"
 #include "Vector.h"
 
 class SpringForceGenerator : public ForceGenerator {
 private:
-    Particle* other; 
+    Particle* other;
     float springConstant;
-    float restLength;  
+    float restLength;
 
 public:
-    // Constructeur
-    SpringForceGenerator(Particle* other, float springConstant, float restLength)
-        : other(other), springConstant(springConstant), restLength(restLength) {}
-
-    // Appliquer la force de ressort 
-    void applyForce(Particle& particle) override {
-        Vector force = particle.position - other->position;
-
-        float distance = force.magnitude();
-        float magnitude = -springConstant * (distance - restLength);
-
-        force = force.normalize();
-
-        particle.applyForce(force.x * magnitude, force.y * magnitude, force.z * magnitude, 0.f);
-    }
+    SpringForceGenerator(Particle* other, float springConstant, float restLength);
+    void applyForce(Particle& particle) override;
 };
