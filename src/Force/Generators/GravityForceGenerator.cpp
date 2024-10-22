503d7310feb22743db8d29db63273fe34e09f4a6
library-rat <1ucas_z@protonmail.com>
2024-10-22 17:08:42 -0400

    Adding macros for default values in gravity

--- src/Force/Generators/GravityForceGenerator.cpp
+++ src/Force/Generators/GravityForceGenerator.cpp
@@ -1,8 +1,12 @@
 #include "GravityForceGenerator.h"
 
 GravityForceGenerator::GravityForceGenerator(float gravity) {
-    _gravity = Vector(0.f, gravity, 0.f);
+    _gravity = DEFAULT_GRAVITY_DIRECTION *gravity;
 }
+
+GravityForceGenerator::GravityForceGenerator(){
+     _gravity = DEFAULT_GRAVITY_DIRECTION*DEFAULT_GRAVITY;
+    }
 
 void GravityForceGenerator::UpdateForce(std::shared_ptr<IObject>& object) {
     if (object == nullptr || object->get_inv_mass() == 0.0f) {
