c4f7965805e2fa19586141dc6cd47bc0a326a9f7
library-rat <1ucas_z@protonmail.com>
2024-10-22 23:20:15 -0400

    adding comment for GravityForceGenerator

--- src/Force/Generators/GravityForceGenerator.h
+++ src/Force/Generators/GravityForceGenerator.h
@@ -1,9 +1,9 @@
 #pragma once
 #include "../IParticleForceGenerator.h"
 
-#define DEFAULT_GRAVITY 1.f
+#define DEFAULT_GRAVITY 1.f //Define the default value of the gravity
 
-#define DEFAULT_GRAVITY_DIRECTION Vector(0,1,0)
+#define DEFAULT_GRAVITY_DIRECTION Vector(0,1,0) //Unitary vector, define the direction of gravity
 
 class GravityForceGenerator : public IParticleForceGenerator {
 public:
