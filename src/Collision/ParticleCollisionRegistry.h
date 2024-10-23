75ced1cb61f8df5af8c4f75beaccfc91ffe9c0c8
library-rat <1ucas_z@protonmail.com>
2024-10-22 23:08:48 -0400

    adding comments to ParticleCollisionRegistry

--- src/Collision/ParticleCollisionRegistry.h
+++ src/Collision/ParticleCollisionRegistry.h
@@ -5,36 +5,36 @@
 #include "../Objects/Particle.h"
 
 class ParticleCollisionRegistry {
-    struct ParticleCollisionEntry {
+    struct ParticleCollisionEntry {//Struct used to store a interpenetration collision Checked in CheckCollision
         std::shared_ptr<Particle> particleA;
         std::shared_ptr<Particle> particleB;
         float restCoeff; //coefficient of restitution give the elasticity of collision : 0 perfectly inelastic, 1 perfectly elastic
     };
 
-    struct ParticleCollisionLinkEntry {
+    struct ParticleCollisionLinkEntry {//Struct used to store a cable or rod collision Checked in CheckCollision 
         std::shared_ptr<Particle> particleA;
         std::shared_ptr<Particle> particleB;
         float length;
     };
 
-    std::vector<ParticleCollisionLinkEntry> RodRegistry;
-    std::vector<ParticleCollisionLinkEntry> CableRegistry;
+    std::vector<ParticleCollisionLinkEntry> RodRegistry; //List of RodCollision registered
+    std::vector<ParticleCollisionLinkEntry> CableRegistry; // List of CableCollision registered
     std::vector<ParticleCollisionEntry> InterRegistry; //List of interpenetrationCollision registered
 
 public:
-    ParticleCollisionRegistry(shared_ptr<ObjectForceRegistry> force_registry);
+    ParticleCollisionRegistry(shared_ptr<ObjectForceRegistry> force_registry); 
 
-    void AddRodCollision(std::shared_ptr<Particle> particleA, std::shared_ptr<Particle> particleB, float length);
-    void AddCableCollision(std::shared_ptr<Particle> particleA, std::shared_ptr<Particle> particleB, float length);
-    void AddInterCollision(std::shared_ptr<Particle> particleA, std::shared_ptr<Particle> particleB, float length);
-    //Add a collision listener between particleA and B of type interpenetrationCollision
+    void AddRodCollision(std::shared_ptr<Particle> particleA, std::shared_ptr<Particle> particleB, float length);//Add a collision listener between particleA and B of type RodCollision
+    void AddCableCollision(std::shared_ptr<Particle> particleA, std::shared_ptr<Particle> particleB, float length);//Add a collision listener between particleA and B of type CableCollision
+    void AddInterCollision(std::shared_ptr<Particle> particleA, std::shared_ptr<Particle> particleB, float length);//Add a collision listener between particleA and B of type interpenetrationCollision
 
-    void RemoveCableCollision(std::shared_ptr<Particle> particleA, std::shared_ptr<Particle> particleB);
+    void RemoveCableCollision(std::shared_ptr<Particle> particleA, std::shared_ptr<Particle> particleB);//Delete a listener of type CableCollision
 
-    void CheckCollision(float duration);
+    void CheckCollision(float duration);//Iterate on all Collision's listeners to check if there is a collision. The argument is the time since last frame for debugging purpose.
 
 private :
-    void CheckRodCollision();
+//Call adapted Handler in case of collision
+    void CheckRodCollision(); 
     void CheckCableCollision();
     void CheckInterCollision();
 
