7ee3b0e8830f70a253146d15a8ff458f9afb21b3
Matheo Ducrot <matheo.ducrot@epita.fr>
2024-11-13 14:27:34 -0500

    box

--- src/Objects/Box.cpp
+++ src/Objects/Box.cpp
@@ -1,8 +1,12 @@
-//
-//  Box.cpp
-//  MPJV_proj
-//
-//  Created by Mathéo Ducrot on 13/11/2024.
-//
+#include "Box.h"
 
-#include <stdio.h>
+Box::Box(float x, float y, float z, float height, float width, float depth, float mass,float inertia) : RigidBody(x, y, z, mass, inertia), height_(height), width_(width), depth_(depth) {
+}
+    
+Box::Box(float x, float y, float z, float height, float width, float depth, float mass, float inertia, ofColor color) : RigidBody(x, y, z, mass, inertia, color), height_(height), width_(width), depth_(depth) {
+}
+
+void Box::draw() {
+    ofSetColor(color_);
+    ofDrawRectangle(position_.x, position_.y, position_.z, width_, height_);
+}
--- src/Objects/Box.h
+++ src/Objects/Box.h
@@ -1,12 +1,15 @@
-//
-//  Box.h
-//  MPJV_proj
-//
-//  Created by Mathéo Ducrot on 13/11/2024.
-//
+#pragma once
+#include "RigidBody.h"
 
-#ifndef Box_h
-#define Box_h
-
-
-#endif /* Box_h */
+class Box : public RigidBody {
+public:
+    Box(float x, float y, float z, float height, float width, float depth, float mass,float inertia);
+    Box(float x, float y, float z, float height, float width, float depth, float mass, float inertia, ofColor color);
+    
+    void draw() override;
+    
+protected:
+    float width_;
+    float height_;
+    float depth_;
+};
