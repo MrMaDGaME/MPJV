c98194ebdb952d96735f996c0eee10783b6c23b9
library-rat <1ucas_z@protonmail.com>
2024-11-13 14:38:41 -0500

    Turning inertia to matrix and correcting compiling error

--- src/Objects/RigidBody.cpp
+++ src/Objects/RigidBody.cpp
@@ -1,50 +1,67 @@
 #include "RigidBody.h"
 
-RigidBody::RigidBody(float x, float y, float z, float mass,float inertia){
+RigidBody::RigidBody(float x, float y, float z, float mass,Matrix3x3 inertia){
     if (mass <= 0.f) {
         throw std::invalid_argument("Mass must be positive");
     }
     position_ = Vector (x,y,z);
     velocity_ = Vector(0,0,0);
     inv_mass_ = 1/mass;
-    inertia_ = inertia; 
+    inv_inertia_ = inertia.inverse(); 
 }
     
-RigidBody::RigidBody(float x, float y, float z, float mass, float intertia, ofColor color){
+RigidBody::RigidBody(float x, float y, float z, float mass, Matrix3x3 inertia, ofColor color){
         if (mass <= 0.f) {
         throw std::invalid_argument("Mass must be positive");
     }
     position_ = Vector (x,y,z);
     velocity_ = Vector(0,0,0);
     inv_mass_ = 1/mass;
-    inertia_ = inertia; 
+    inv_inertia_ = inertia.inverse(); 
 }
 
-//    void update() override;
+//    void update() ;
 
-void RigidBody::draw() {
-    ofSetColor(color_);
-    ofDrawRectangle(position_.x, position_.y, position_.z, width_, height_);
-}
-void RigidBody::addForce(const Vector& force) override{
+// void RigidBody::draw() {
+//     ofSetColor(color_);
+//     ofDrawRectangle(position_.x, position_.y, position_.z, width_, height_);
+// }
+void RigidBody::addForce(const Vector& force) {
         accum_force_ += force;
 }
-    void addForce(const Vector& applyPoint, const Vector& force){
+void RigidBody::addForce(const Vector& applyPoint, const Vector& force){
         accum_force_ += force;
+        Vector l =  -  position_  + applyPoint;
+        accum_torque +=  l^force;
     }
 
 
-    [[nodiscard]] Vector get_position() const override;
-    void set_position(const Vector& position) override;
-    [[nodiscard]] Vector get_velocity() const override;
-    void set_velocity(const Vector& velocity) override;
-    [[nodiscard]] float get_inv_mass() const override;
-    void set_inv_mass(float inv_mass) override;
-    virtual void fill_object_collision(std::shared_ptr<IObject> other,
-                                       std::shared_ptr<ParticleCollisionRegistry>& collision_registry,
-                                       CollisionType collision_type,
-                                       float coeff) override;
-    void fill_particle_collision(std::shared_ptr<Particle> particle,
-                                         std::shared_ptr<ParticleCollisionRegistry>& collision_registry,
-                                         CollisionType collision_type,
-                                         float coeff) override;
+[[nodiscard]] Vector RigidBody::get_position() const {
+    return position_;
+}
+void RigidBody::set_position(const Vector& position) {
+    this->position_ = position;
+}
+[[nodiscard]] Vector RigidBody::get_velocity() const {
+    return velocity_;
+}
+
+void RigidBody::set_velocity(const Vector& velocity) {
+    this->velocity_ = velocity;
+}
+
+[[nodiscard]] float RigidBody::get_inv_mass() const {
+    return inv_mass_;
+}
+
+void RigidBody::set_inv_mass(float inv_mass) {
+    this->inv_mass_ = inv_mass;
+}
+    // virtual void fill_object_collision(std::shared_ptr<IObject> other,
+    //                                    std::shared_ptr<ParticleCollisionRegistry>& collision_registry,
+    //                                    CollisionType collision_type,
+    //                                    float coeff) ;
+    // void fill_particle_collision(std::shared_ptr<Particle> particle,
+    //                                      std::shared_ptr<ParticleCollisionRegistry>& collision_registry,
+    //                                      CollisionType collision_type,
+    //                                      float coeff) ;
--- src/Objects/RigidBody.h
+++ src/Objects/RigidBody.h
@@ -6,11 +6,11 @@
 class RigidBody : public IObject, public std::enable_shared_from_this<RigidBody> {
 
 protected:
-    RigidBody(float x, float y, float z, float mass,float inertia);
-    RigidBody(float x, float y, float z, float mass, float intertia, ofColor color);
+    RigidBody(float x, float y, float z, float mass,Matrix3x3 inertia);
+    RigidBody(float x, float y, float z, float mass, Matrix3x3 inertia, ofColor color);
 public:
     void update() override;
-    void draw() override;
+    virtual void draw() override;
     void addForce(const Vector& force) override;
     void addForce(const Vector& applyPoint, const Vector& force);
 
@@ -34,6 +34,7 @@ protected:
     Vector position_;
     Vector velocity_;
     Vector accum_force_;
+    Vector accum_torque;
     float inv_mass_;
     Matrix3x3 inv_inertia_;
     ofColor color_;
